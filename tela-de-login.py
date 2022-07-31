4e024ed72646ca134371bdf2b3546c4e18ad444b
Marcell Dactes <83461178+marcelldac@users.noreply.github.com>
2022-07-31 15:36:56 -0300

    Add files via upload

---
+++ tela-de-login.py
@@ -0,0 +1,23 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('User'),sg.Input(key='usuario')],
+    [sg.Text('Pass'),sg.Input(key='senha',password_char='*')],
+    [sg.Checkbox('Remember this?')],
+    [sg.Button('Login')]
+]
+
+
+janela = sg.Window('Login', layout)
+
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Login':
+        if valores['usuario'] == 'Marcelldac' and valores['senha'] == 'marcelldac':
+            print('Bem-vindo ao login')
+    
