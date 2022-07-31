0747fcd6a243bd327bd91040e97a1ecf7144d7f8
Marcell Dactes <83461178+marcelldac@users.noreply.github.com>
2022-07-31 16:45:12 -0300

    versão 1.1
    modificação: agora o usuário que não está indicado no código é recebido com uma mensagem de 'você não é bem vindo'

--- tela-de-login.py
+++ tela-de-login.py
@@ -2,7 +2,7 @@ from PySimpleGUI import PySimpleGUI as sg
 
 
 sg.theme('Reddit')
-layout = [
+layout = [  
     [sg.Text('User'),sg.Input(key='usuario')],
     [sg.Text('Pass'),sg.Input(key='senha',password_char='*')],
     [sg.Checkbox('Remember this?')],
@@ -20,4 +20,6 @@ while True:
     if eventos == 'Login':
         if valores['usuario'] == 'Marcelldac' and valores['senha'] == 'marcelldac':
             print('Bem-vindo ao login')
+        else:
+            print('Você não é bem vindo')
     
